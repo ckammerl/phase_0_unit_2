ac14a59e3644672d1cbed7f503b6f42183b3354b
Christiane Kammerl <christiane@kammerl.com>
2014-04-03 22:18:25 -0700

    added solution

--- week_4/my_solutions/3_commas_solution.rb
+++ week_4/my_solutions/3_commas_solution.rb
@@ -1,24 +1,109 @@
 # U2.W4: 3_commas_solution.rb
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 # 2. Pseudocode
 
-# Input: strings and integer
-# Output: string (template)
+# Input: integer
+# Output: integer comma-separted returned as string
 # Steps:
+=begin
+WRITE a method seperate_comma which takes an num (integer) as input
+SET num_as_string equal to calling to_s on num
+SET num_as_arr_of_strings equal to calling split(//) on num_as_string
+SET num_len equal to calling .length on num_as_string
+
+WHEN num_len < 3
+	set result equal to num_as_string
+
+WHEN num_len equal 4
+	set var a equal to inserting a comma as string to num_as_arr_of_strings at index 0
+	set result equal to calling the method join on var a 
+
+WHEN num_len equal 5
+	set var b equal to inserting a comma as string to num_as_arr_of_strings at index 1
+	set result equal to calling the method join on var b
+
+WHEN num_len equal 6
+	set var c equal to inserting a comma as string to num_as_arr_of_strings at index 2
+	set result equal to calling the method join on var c
+
+WHEN num_len iequal 7
+	reuse var a
+	set var d equal to inserting a comma as string to var a at index 5
+	set result equal to calling the method join on var d
+END
+
+RETURN result
+=end
 
 
 # 3. Initial Solution
+def seperate_comma(num)
+	num_as_string = num.to_s
+	num_as_arr_of_strings = num_as_string.split(//)
+	num_len = num_as_string.length
 
+	case num_len
+	when 0...4
+		result = num_as_string
+	when 4
+		a = num_as_arr_of_strings.insert(1, ",")
+		result = a.join(" ")
+	when 5
+		b = num_as_arr_of_strings.insert(2, ",")
+		result = b.join(" ") 
+	when 6
+		c = num_as_arr_of_strings.insert(3, ",")
+		result = c.join(" ")
+	when 7
+		a = num_as_arr_of_strings.insert(1, ",")
+		d = a.insert(5, ",")
+		result = d.join(" ")
+	end
 
-
+	return result
+end 
 
 # 4. Refactored Solution
 
+def seperate_comma(num)
+  num_as_string = num.to_s
+  num_as_arr_of_strings = num_as_string.split(//) #["1", "2" "3"]
+  num_len = num_as_string.length
+
+      if (0..3).include?(num_len)
+        result = num_as_string
+      elsif
+         (4..6).include?(num_len)
+       	i = num_len - 3
+        a = num_as_arr_of_strings.insert(i, ",")
+        result = a.join("")
+      else
+      	b = num_as_arr_of_strings.insert(1, ",")
+		c = b.insert(5, ",")
+		result = c.join("")
+       end
+    return result
+ end
+
+p seperate_comma(0)
+p seperate_comma(100)
+p seperate_comma(1000) 
+p seperate_comma(400444) 
+p seperate_comma(9999111) 
+
+
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
+
+p seperate_comma(100).class == String
+p seperate_comma(100) == "100"
+p seperate_comma(1000) == "1,000"
+p seperate_comma(40000) == "40,000"
+p seperate_comma(400444) == "400,444"
+p seperate_comma(9999111) == "9,999,111"  
 
 
 # 5. Reflection 
