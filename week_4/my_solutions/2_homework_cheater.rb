d3ff52877f848b221e85be731d33c5f5941d8564
Christiane Kammerl <christiane@kammerl.com>
2014-04-03 19:46:35 -0700

    added final solution

--- week_4/my_solutions/2_homework_cheater.rb
+++ week_4/my_solutions/2_homework_cheater.rb
@@ -53,7 +53,7 @@ def essay_writer(title, subject, year, thesis_statement)
 
 	count_thesis_statement = thesis_statement.split(/ /)
 	if count_thesis_statement.length > 20
-		formated_thesis_statement = count_thesis_statement.each_slice(10).to_a { |x| x.push("\n") }.join(" ")
+		formated_thesis_statement = count_thesis_statement.each_slice(10).to_a { |x| x.push("\n") }.join(" ")  #code not working properly
 
 	else
 		sorted_thesis_statement = thesis_statement
@@ -76,12 +76,112 @@ puts essay_writer("famous scientists", "marie curie", 1903, "was a Polish and na
 who conducted pioneering research on radioactivity. She was the first woman to win a Nobel Prize in 1903. She was also the first woman to become a professor
 at the University of Paris, and in 1995 became the first woman to be entombed on her own merits in the Panthéon in Paris.")
 
+
 # 4. Refactored Solution
 
+def essay_writer(title, subject, year, thesis_statement)
+	upcase_title = title.upcase
 
-#TBD	
+	if year.to_s.length != 4
+		puts "The year should be an integer and written YYYY."
+	end 
+
+	if subject.split.length == 2
+		capitalized_subject = subject.split.map { |y| y.capitalize }.join(" ")
+	else 
+		capitalized_subject = subject.capitalize 
+	end 
+
+	count_thesis_statement = thesis_statement.split(/ /)
+	if count_thesis_statement.length > 20
+	    enum_with_array_of_10 = count_thesis_statement.each_slice(10) # step by step, code now working
+	    array_with_array_of_10 = enum_with_array_of_10.to_a
+	    array_with_array_of_10_with_nl = array_with_array_of_10.each { |x| x.push("\n") }
+	    formated_thesis_statement = array_with_array_of_10_with_nl.join(" ")
+	else
+		formated_thesis_statement = thesis_statement
+	end 
+
+	template = "Essay by me\n#{upcase_title}\n\nThe year #{year} is an important date when talking about #{capitalized_subject}.\s#{capitalized_subject}\s#{formated_thesis_statement}"
+
+	return template
+end
+
+# I looked up how to write multiple line strings using "TEXT"\
+puts essay_writer("famous discoverer", person1, "(born between October 31, 1450 and October 30, 1451 – 20 May 1506) was an Italian explorer, "\
+                                                      "navigator, and colonizer, born in the Republic of Genoa (Italy). Under the auspices ofthe Catholic "\
+                                                      "Monarchs of Spain, he completed four voyages across the Atlantic Ocean that led to general "\
+                                                      "European awareness of the American continents. Those voyages, and his efforts to establish "\
+                                                      "permanent settlements on the island of Hispaniola, initiated the Spanish colonization of the New World.")
+
+puts " "
+
+puts essay_writer("famous scientists", person2, "was a Polish and naturalized-French physicist and chemist (7 November 1867 – 4 July 1934) who conducted pioneering "\
+													  "research on radioactivity. She was the first woman to win a Nobel Prize in 1903. She was also the first woman to become "\
+													  "a professor at the University of Paris, and in 1995 became the first woman to be entombed on her own merits in the Panthéon in Paris.")
 
 
+
+
+# Different solution using a class Person; After re-reading the instructions, I thought writing a class AND the method should be the best
+# solution. I wrote the code after watching the dedicated Ruby tutorial at Treehouse and discussing the class concept again with a friend
+
+class Person
+	def initialize(name, pronoun, year)  # pronoun = "He" oder "She" oder "It"
+		@firstname = name.split.first.capitalize
+		@lastname = name.split.last.capitalize
+		@pronoun = pronoun
+		@year = year
+		if check_year(year) == false
+			puts "The year should be an integer and written YYYY."
+			exit
+		end 
+	end 
+
+	def getname()
+		name = @firstname+" "+@lastname
+        return name
+	end
+
+	def getpronoun()
+		return @pronoun
+	end
+
+	def get_year()
+		return @year
+	end 
+
+	def check_year(year)
+		if year.to_s.length != 4
+			return false
+		else 
+			return true
+		end 
+	end 
+end 
+
+	
+def essay_writer(title, subject, thesis_statement)
+	upcase_title = title.upcase
+
+	count_thesis_statement = thesis_statement.split(/ /)
+	if count_thesis_statement.length > 20		
+		# step by step
+	    enum_with_array_of_10 = count_thesis_statement.each_slice(10)
+	    array_with_array_of_10 = enum_with_array_of_10.to_a
+	    array_with_array_of_10_with_nl = array_with_array_of_10.each { |x| x.push("\n") }
+	    formated_thesis_statement = array_with_array_of_10_with_nl.join(" ")
+	else
+		formated_thesis_statement = thesis_statement
+	end 
+
+	template = "Essay by me\n#{upcase_title}\n\nThe year #{subject.get_year} is an important date when talking about #{subject.getname}.\n#{subject.getpronoun} #{formated_thesis_statement}"
+
+	return template
+end
+
+person1 = Person.new("Christoph Columbus", "He", 1492)
+person2 = Person.new("Marie Curie", "She", 1903)
 
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
@@ -115,7 +215,46 @@ TEST
 
 # 5. Reflection 
 
-#TBD
+=begin
+What parts of your strategy worked? What problems did you face?
+After reading the directions, especially the lines: 
+"Identify and pass in a person/place/topic into the template" and
+"Use the appropriate pronoun for a person (male or female), place, or thing (HINT: this may be a good place to try creating an object...)"
+my first thought was, I should use a class to solve this challenge. But I also know me and that I sometimes tend to concentrate too much
+and too long on details in the instructions and then end up thinking too complicated. Therefore, I first wrote the initial solution. I 
+started with diver test. I am not sure I understood the driver code instructions correctly
+but I wrote them the way they made sense to me. I defined the desired output first and stored it in the var template, then I 
+worte 3 vars and set each equal to the result of calling the method essay_writer on these vars. The test actually follows all these steps. I have some trouble with coming up with good
+driver code and in this case, I was not 100% clear on the instructions.
+
+What questions did you have while coding? What resources did you find to help you answer them?
+
+String: "Text"\ = how to write a multi-line string: http://stackoverflow.com/questions/2337510/ruby-can-i-write-multi-line-string-with-no-concatenation
+Array: I went back to ruby-doc.org and re-read about .slice() {} - I will need further reading (using the method) to fully understand
+		the return value which is Enumerator and how to use that class
+String: "string".split.map {} = split string to get an array and then do sth to all its element: http://stackoverflow.com/questions/13520162/ruby-capitalize-every-word-first-letter
+
+What concepts are you having trouble with, or did you just figure something out? If so, what?
+I wanted to write a second solution and to use a class Person together with my method essay_writer; to understand the concept better, I watched:
+Video Tutorial: Objects, Classes, Variables http://teamtreehouse.com/library/ruby-foundations#ruby-basics
 
 
+Did you learn any new skills or tricks?
+Absolutely! I invested some time to get a better understanding of building and using a class! The video tutorial helped, it was a good
+refresher, and then I started writing the class Person. However, I needed to discuss how to use the instances of the class in my method with a 
+friend who explained it to me. I don't think I will write my next solution using a class without facing any trouble, but 
+I feel much better about how to build and use classes.
 
+How confident are you with each of the learning objectives?
+Translate a user story into driver code and solutions - I am not sure I understood the driver code instructions correctly
+but I wrote them the way they made sense to me. 
+
+Create methods that take arguments to modify the output - confident
+Use if/else statements, string methods, while/until loops, Enumerable#each methods - confident
+Differentiate between and produce destructive and non-destructive methods - confident
+
+
+Which parts of the challenge did you find tedious?
+I found the instructions/ given directions not to be completely clear.
+
+=end
